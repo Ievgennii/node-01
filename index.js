14fdf7bb78ee0c92fd8603bbbfe625fe9f4e892d
Ievgennii <simonovievgenii@gmail.com>
2023-07-06 01:13:27 +0300

    finished

--- index.js
+++ index.js
@@ -1,4 +1,17 @@
 const contacts = require("./contacts")
+const { Command } = require('commander');
+
+const program = new Command();
+program
+  .option('-a, --action <type>', 'choose action')
+  .option('-i, --id <type>', 'user id')
+  .option('-n, --name <type>', 'user name')
+  .option('-e, --email <type>', 'user email')
+  .option('-p, --phone <type>', 'user phone');
+
+program.parse(process.argv);
+
+const argv = program.opts();
 
 const invokeAction = async ({action, id, name, email, phone}) => {
   switch(action) {
@@ -15,10 +28,20 @@ const invokeAction = async ({action, id, name, email, phone}) => {
       case "remove":
           const removeContact = await contacts.removeContact(id);
           return console.log(removeContact);
+
+      default:
+        console.warn('\x1B[31m Unknown action type!');
   }
 }
 
 // invokeAction({action: "list"});
 // invokeAction({action: "get", id: "rsKkOQUi80UsgVPCcLZZW"});
 // invokeAction({action: "add", name: "Eugene Simonov", email: "es@ukr.net", phone: "34535435"});
-invokeAction({action: "remove", id: "r28Flb2XL7xe3sqc8zo5J"});
+// invokeAction({action: "remove", id: "r28Flb2XL7xe3sqc8zo5J"});
+
+invokeAction(argv);
+
+// node index -a list
+// node index -a get -i rsKkOQUi80UsgVPCcLZZW
+// node index -a add -n Din Rid -e dr@ukr.net -p 00000000000
+// node index -a remove -i Q_CyjEaD9ICb7eM6enx38
